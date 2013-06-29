ba4fb58817fa2de9e3fd39a320a7b9a8d11ea467
Dirk Melchers <dirk@dirk-melchers.de>
2013-06-30 01:07:41 +0200

    SHM zum Speichern der Bilder verwenden

--- HausMessung/make_graphs/rrd_graph.pl
+++ HausMessung/make_graphs/rrd_graph.pl
@@ -5,10 +5,14 @@ use CGI;
 
 $RRD::Simple::DEBUG=0;
 $|=1;
+if (!-d "/run/shm/HausMessung/img/") {
+	system("mkdir -p /run/shm/HausMessung/img/");
+}
 
 my $q = new CGI;
 
-my $dest = "/home/pi/Arduino/HausMessung/img";
+#my $dest = "/home/pi/Arduino/HausMessung/img";
+my $dest = "/run/shm/HausMessung/img";
 #my @periods = qw( hour 6hour 12hour day week month year 3years);
 my @periods = qw( hour day week );
 my %trends = ("hour"=>600, "6hour"=>1800, "12hour"=>2400, "day"=>3600, "week"=>3600*12, "month"=>3600*24*2, "year"=>3600*24*10, "3years"=>3600*24*30);
@@ -49,7 +53,7 @@ my %dsnames_luefter = map { $_ => 1 } @dsnames_luefter;
 @wanted_strom = grep { $dsnames_strom{$_} } @wanted_strom;
 @wanted_luefter = grep { $dsnames_luefter{$_} } @wanted_luefter;
 
-my $mtime=(stat "$dest/temp/aquarium-hourly.png")[9];
+my $mtime=(stat "$dest/aquarium-hourly.png")[9];
 
 if (time() - $mtime > 120) {
 foreach my $period (@periods) {
@@ -57,17 +61,19 @@ foreach my $period (@periods) {
 
 #	print "Generate temps $period\n";
 	$rrd_temp->graph(
-	        destination => "$dest/temp",
+	        destination => "$dest",
        	 	extended_legend => 1,
 		periods => [ $period ],
 		sources => [ grep { $_ ne "KGHeizung" } @wanted_temp ],
 		width=>500,
 		height=>200,
+		#upper_limit => 35,
+         	#lower_limit => 15,
 	);
 
 #	print "Generate heating $period\n";
 	$rrd_temp->graph(
-        	destination => "$dest/temp",
+        	destination => "$dest",
         	extended_legend => 1,
 		periods => [ $period ],
 		sources => [ "KGHeizung" ],
@@ -78,18 +84,20 @@ foreach my $period (@periods) {
 
 #        print "Generate aquarium $period\n";
         $rrd_temp->graph(
-                destination => "$dest/temp",
+                destination => "$dest",
                 extended_legend => 1,
                 periods => [ $period ],
                 sources => [ "Aquarium" , "AquariumAussen"],
                 basename => "aquarium",
                 width=>500,
                 height=>200,
+		#upper_limit => 35,
+         	#lower_limit => 15,
                 );
 
 #	print "Generate luefter $period\n";
 	$rrd_luefter->graph(
-        	destination => "$dest/luefter",
+        	destination => "$dest",
         	extended_legend => 1,
 		periods => [ $period ],
 		sources => [ qw(Luefter) ],
@@ -100,7 +108,7 @@ foreach my $period (@periods) {
 
 #    print "Generate strom $period\n";
 #    $rrd_strom->graph(
-#                destination => "$dest/strom",
+#                destination => "$dest",
 #                extended_legend => 1,
 #                periods => [ $period ],
 #                sources => [ @wanted_strom ],
@@ -115,21 +123,25 @@ foreach my $period (@periods) {
 }
 }
 
-print $q->header(), $q->start_html();
+print $q->header('Hausmessung'), $q->start_html('Hausmessung');
 print << "_EOF_";
 <h1>Hausmessung</H1>
 <h3>Aquarium</h3>
-<img src="/hausmessung/img/temp/aquarium-hourly.png">
-<img src="/hausmessung/img/temp/aquarium-daily.png">
-<img src="/hausmessung/img/temp/aquarium-weekly.png"><br>
+<img src="/hausmessung/img/aquarium-hourly.png">
+<img src="/hausmessung/img/aquarium-daily.png">
+<img src="/hausmessung/img/aquarium-weekly.png"><br>
 <H3>Temperaturen</H3>
-<img src="/hausmessung/img/temp/temperatur-hourly.png">
-<img src="/hausmessung/img/temp/temperatur-daily.png">
-<img src="/hausmessung/img/temp/temperatur-weekly.png"><br>
+<img src="/hausmessung/img/temperatur-hourly.png">
+<img src="/hausmessung/img/temperatur-daily.png">
+<img src="/hausmessung/img/temperatur-weekly.png"><br>
 <h3>L&uuml;fter</h3>
-<img src="/hausmessung/img/luefter/luefter-hourly.png">
-<img src="/hausmessung/img/luefter/luefter-daily.png">
-<img src="/hausmessung/img/luefter/luefter-weekly.png"><br>
+<img src="/hausmessung/img/luefter-hourly.png">
+<img src="/hausmessung/img/luefter-daily.png">
+<img src="/hausmessung/img/luefter-weekly.png"><br>
+<h3>Heizung</h3>
+<img src="/hausmessung/img/heizung-hourly.png">
+<img src="/hausmessung/img/heizung-daily.png">
+<img src="/hausmessung/img/heizung-weekly.png"><br>
 _EOF_
 print $q->end_html();
 
